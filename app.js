be37601f5a9f9fe44c060dd7415f6989b3313a5f
Anthony Holloman <anthony.holloman1@t-mobile.com>
2022-06-01 16:25:25 -0500

    updating route for tests

--- app.js
+++ app.js
@@ -10,24 +10,24 @@ let books = [];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/book", (req, res) => {
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/book", (req, res) => {
+  const sortedBooks = [...books];
+  sortedBooks.sort((a, b) => a.title - b.title);
+  res.json(sortedBooks);
+});
+
+app.post("/api/book", (req, res) => {
   const book = req.body;
   books.push({ id: books.length + 1, ...book });
 
   res.send(books[books.length - 1]);
 });
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/book", (req, res) => {
-  const sortedBooks = [...books];
-  sortedBooks.sort((a, b) => a.title - b.title);
-  res.json(sortedBooks);
-});
-
-app.delete("/book", (req, res) => {
+app.delete("/api/book", (req, res) => {
   books = [];
   res.send(204);
 });
