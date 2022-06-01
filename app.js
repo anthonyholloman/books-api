7de725ca075f0591f7d9b7c3056d83caa7d997b4
Anthony Holloman <anthony.holloman1@t-mobile.com>
2022-06-01 16:29:20 -0500

    routes

--- app.js
+++ app.js
@@ -14,20 +14,20 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/book", (req, res) => {
+app.get("/api/books", (req, res) => {
   const sortedBooks = [...books];
   sortedBooks.sort((a, b) => a.title - b.title);
   res.json(sortedBooks);
 });
 
-app.post("/api/book", (req, res) => {
+app.post("/api/books", (req, res) => {
   const book = req.body;
   books.push({ id: books.length + 1, ...book });
 
   res.send(books[books.length - 1]);
 });
 
-app.delete("/api/book", (req, res) => {
+app.delete("/api/books", (req, res) => {
   books = [];
   res.send(204);
 });
