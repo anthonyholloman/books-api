26d4d15fb6d278f6ca2561c26f27f6b6ee1fb8df
anthonyholloman <35376609+anthonyholloman@users.noreply.github.com>
2022-06-01 14:19:31 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.status(200).send("Don't panic.");
+});
+
+let books = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/book", (req, res) => {
+  const book = req.body;
+  books.push({ id: books.length + 1, ...book });
+
+  res.send(books[books.length - 1]);
+});
+
+app.get("/book", (req, res) => {
+  const sortedBooks = [...books];
+  sortedBooks.sort((a, b) => a.title - b.title);
+  res.json(sortedBooks);
+});
+
+app.delete("/book", (req, res) => {
+  books = [];
+  res.send(204);
+});
+
+module.exports = app;
