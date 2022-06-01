ef6e53e8b45006a2759f93a8df283afa2b4009f8
Anthony Holloman <anthony.holloman1@t-mobile.com>
2022-06-01 15:34:04 -0500

    updating port to be dynamic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const port = 5000;
-const app = require('./app');
+const port = process.env.PORT || 5000;
+const app = require("./app");
 
 app.listen(port, () => {
   console.log(`Server is running, listening on port ${port}`);
